a885bc3fd95c4532bf880058fb03b5f2ff5d928b
pawayy <psway1@buffs.wtamu.edu>
2025-01-27 20:11:47 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[ ] args)
+    {
+    var _valueX = 2.5;
+    var _valueY = 3.3;
+    var _valueZ = (4*_valueX*_valueX)+(3*_valueY);
+    Console.WriteLine($"X=2.5,Y= 3.3 \nThe value of Z is:{_valueZ}");
+ 			     }
+}
